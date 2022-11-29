0e1fbaa59db0941ce093d7c50586b5ff21b6613e
junuuuuuu <junuuu@i-nara.co.kr>
2022-11-29 22:32:57 +0900

    feat: 프로젝트 세팅

---
+++ src/main/kotlin/com/kidsworld/clientdemo/ClientdemoApplication.kt
@@ -0,0 +1,11 @@
+package com.kidsworld.clientdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClientdemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClientdemoApplication>(*args)
+}
